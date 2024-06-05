a94487ac9ec34d375752c78bb779ba6e60e7a291
diegobarreto18 <dbarreto9601@gmail.com>
2024-06-05 19:45:21 -0300

    autenticando-bien

--- redes-t3/TCP/cliente.py
+++ redes-t3/TCP/cliente.py
@@ -1,28 +1,44 @@
 import socket
 import sys
 import time
+import hashlib
 
-print("Cliente Hora 2024")
 
-msg = input("fecha/hora: ")
+print("Cliente.py")
+
+nombre = input('Ingrese nombre \n').strip() 
+password = input('Ingrese contraseña \n').strip()
+
+def password_a_md5(password):
+    if not password:
+        return None
+    return hashlib.md5(password.encode('utf-8')).hexdigest()
+
+password_a_md5 = password_a_md5(password)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-client_socket.connect((sys.argv[1], int(sys.argv[2])))
-
-time.sleep(1)
+client_socket.connect((sys.argv[2], int(sys.argv[3])))
 
 msg_in = client_socket.recv(1024).decode('utf-8').strip()
 
-if msg_in == "Redes – Servidor Hora":
-	time.sleep(1)
+"{nombre}-{clavenemd5}\r\n"
+
+
+if msg_in == 'Redes 2024 - Laboratorio - Autenticacion de Usuarios':
+	msg = nombre + "-" + password_a_md5 + "\r\n"
 	client_socket.send(msg.encode())
+	time.sleep(1)
 	msg_in = client_socket.recv(1024).decode('utf-8').strip()
+
+	#if msg_in == 'SI':
+	#	login = true
+	#	client_socket.close() 
+	#else
+	#	print(f"Usuario incorrecto, cerrando conexión")
+	#client_socket.close()
 	print(msg_in)
 else:
 	print(f"Protocolo Incorrecto")
 client_socket.close()
-
-
 
 
