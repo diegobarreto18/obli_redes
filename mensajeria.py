231184ff096591328553d51c6613363b88761975
brianmontero <brianmontero1234@gmail.com>
2024-06-21 17:57:39 -0300

    implementando archivos

--- mensajeria.py
+++ mensajeria.py
@@ -19,7 +19,7 @@ PUERTO = int(sys.argv[1])
 IP_AUTENTICACION = sys.argv[2]
 PUERTO_AUTENTICACION = int(sys.argv[3])
 
-MI_IP = socket.gethostbyname(socket.gethostname())
+MI_IP = sys.argv[4]
 
 clients = []
 input_queue = queue.Queue()
@@ -85,7 +85,22 @@ def get_destinatario(mensaje: str):
 	data = mensaje.split()
 	return data[0]
 
-def get_mensaje_formateado(mensaje: str):
+def get_archivo(mensaje):
+    destinatario, indicador, path = mensaje.split()
+    archivo_binario = None
+
+    if not path or not os.path.exists(path):
+        return archivo_binario
+    
+    nombre_archivo, extension = os.path.splitext(path)
+
+    with open(path, 'rb') as archivo:
+        archivo_binario = archivo.read()
+
+    nombre_archivo = os.path.splitext(path)
+    return [archivo_binario, nombre_archivo + extension + '\n']
+
+def get_mensaje_formateado(mensaje):
 	division_mensaje = mensaje.split()
 	mensaje_original = ' '.join(division_mensaje[1:])
 	return f"[{datetime.now().strftime('%Y.%m.%d')} {datetime.now().strftime('%H:%M')}] {MI_IP} {nombre_usuario_autenticado} dice: {mensaje_original}".encode()
@@ -99,11 +114,43 @@ def emitir():
     while True:
         mensaje = input_queue.get()
         destinatario = get_destinatario(mensaje)
-        mensaje_completo = get_mensaje_formateado(mensaje)
+        archivo_a_mandar = None
+        nombre_archivo_a_mandar = None
+
+        if mensaje.find('&file') > -1:
+            archivo_a_mandar, nombre_archivo_a_mandar = get_archivo(mensaje)
             
         emisor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         emisor_socket.connect((destinatario, PUERTO))
-        emisor_socket.sendall(mensaje_completo)
+        
+        if bool(archivo_a_mandar):
+            emisor_socket.sendall(b'ARCHIVO\n')
+            emisor_socket.sendall(nombre_archivo_a_mandar.encode())
+            emisor_socket.sendall(archivo_a_mandar)
+        else:
+            mensaje_completo = get_mensaje_formateado(mensaje)
+            emisor_socket.sendall('TEXTO\n'.encode())
+            emisor_socket.sendall(mensaje_completo)
+            
+def guardar_archivo(cliente_socket):
+    nombre_archivo = get_nombre_de_archivo(cliente_socket)
+    path_a_guardar = os.path.join(os.getcwd(), nombre_archivo)
+
+    with open(path_a_guardar, 'wb') as archivo:
+        while True:
+            file_data = cliente_socket.recv(1024)
+            if not file_data:
+                break
+            archivo.write(file_data)
+
+def get_nombre_de_archivo(cliente_socket):
+    nombre_archivo = b''
+    while True:
+        char = cliente_socket.recv(1)
+        if char == b'\n':
+            break
+        nombre_archivo += char
+    nombre_archivo = nombre_archivo.decode()
 
 def recibir(cliente_socket):
     while True:
@@ -112,7 +159,11 @@ def recibir(cliente_socket):
         if not data.decode() or data.decode() == '\r\n':
             continue
 
-        print(data.decode())
+        if data.decode() == 'ARCHIVO\n':
+            guardar_archivo(cliente_socket)
+            print("Archivo guardado")
+        if data.decode() == 'TEXTO\n':
+            print(data.decode())
                 
 def levantar_server():
     while True:
